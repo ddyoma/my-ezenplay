4edfc61f9b2f0c6a3b5b5b13c8cee72c61af45ff
cwyoon75 <75353946+cwyoon75@users.noreply.github.com>
2021-04-07 11:26:22 +0900

    Delete src/main/java/com/spboot/test/repository directory

--- src/main/java/com/spboot/test/repository/PcInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spboot.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spboot.test.entity.PcInfo;
-
-public interface PcInfoRepository extends JpaRepository<PcInfo, Integer> {
-
-}
--- src/main/java/com/spboot/test/repository/UserInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spboot.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spboot.test.entity.UserInfo;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
-
-}
