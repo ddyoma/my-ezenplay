2ef6485779f946a84bd3476e0fbb201e8dbd8af6
yyy <dbsdndwo789@naver.com>
2021-04-07 13:40:42 +0900

    1

--- src/main/java/com/spboot/test/controller/MailController.java
+++ src/main/java/com/spboot/test/controller/MailController.java
@@ -1,5 +1,7 @@
 package com.spboot.test.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,5 +20,15 @@ public class MailController {
 	public Integer sendMail(@RequestBody SimpleMailMessage smm) {
 		mailService.mailSend(smm);
 		return 1;
+	}
+	
+	public static void main(String[] args) {
+		String str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		Random r = new Random();
+		for(int i=1;i<=6;i++) {
+			int rNum = r.nextInt(str.length());
+			char c = str.charAt(rNum);
+			System.out.print(c);
+		}
 	}
 }
